857059bd393baa2e7fd3b3293e7498b6dbb11821
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-06-23 21:21:37 -0300

    Projects Hero Cards

--- src/components/sections/projects/ProjectsHeroSection.js
+++ src/components/sections/projects/ProjectsHeroSection.js
@@ -8,30 +8,32 @@ export default function ProjectsHeroSection() {
     <Wrapper>
       <ProjectWaves />
       <ContentWrapper>
-        <CardsWrapper2>
-          <Card2>
-            <Icon src="/images/illustrations/iPhoneCard.png" width={"348"} />
-          </Card2>
-          <CardBottom2>
-            <Subtitle>SwiftUI for iOS 15</Subtitle>
-          </CardBottom2>
-        </CardsWrapper2>
-        <CardsWrapper>
-          <Card>
-            <Icon src="/images/illustrations/iPhoneCard.png" width={"348"} />
-          </Card>
-          <CardBottom>
-            <Subtitle>SwiftUI for iOS 15</Subtitle>
-          </CardBottom>
-        </CardsWrapper>
-        <CardsWrapper2>
-          <Card2>
-            <Icon src="/images/illustrations/iPhoneCard.png" width={"348"} />
-          </Card2>
-          <CardBottom2>
-            <Subtitle>SwiftUI for iOS 15</Subtitle>
-          </CardBottom2>
-        </CardsWrapper2>
+        <MiddleWrapper>
+          <CardsWrapper2>
+            <Card2>
+              <Icon src="/images/illustrations/macStudio.png" width={"200"} />
+            </Card2>
+            <CardBottom2>
+              <Subtitle>React Hooks</Subtitle>
+            </CardBottom2>
+          </CardsWrapper2>
+          <CardsWrapper>
+            <Card>
+              <Icon src="/images/illustrations/iPhoneCard.png" width={"280"} />
+            </Card>
+            <CardBottom>
+              <Subtitle>SwiftUI for iOS 15</Subtitle>
+            </CardBottom>
+          </CardsWrapper>
+          <CardsWrapper2>
+            <Card2>
+              <Icon src="/images/illustrations/gear.png" width={"200"} />
+            </Card2>
+            <CardBottom2>
+              <Subtitle>Leetcode</Subtitle>
+            </CardBottom2>
+          </CardsWrapper2>
+        </MiddleWrapper>
       </ContentWrapper>
     </Wrapper>
   );
@@ -42,87 +44,123 @@ const Wrapper = styled.div`
 `;
 
 const ContentWrapper = styled.div`
-  max-width: 1234px;
   margin: 0 auto;
   padding-top: 220px;
-  padding-right: 30px;
+  padding-right: 60px;
   padding-bottom: 250px;
-  padding-left: 30px;
+  padding-left: 60px;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+
   overflow: hidden;
-  @media (max-width: 450px) {
-    transform: scale(0.8);
+
+  gap: 30px;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    gap: 120px;
+  }
+`;
+
+const MiddleWrapper = styled.div`
+  max-width: 800px;
+
+  //centering
+  display: flex;
+  align-self: center;
+  align-items: center;
+  align-content: center;
+  justify-content: space-around;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    gap: 120px;
   }
 `;
 
 const CardsWrapper = styled.div`
-  /* padding-top: 80px; */
-  /* padding-bottom: 80px; */
-
-  //centering
   display: flex;
-  /* justify-content: center; */
-
-  transform: scale(1.3);
-  z-index: 5;
 `;
 
 const CardsWrapper2 = styled.div`
-  /* padding-top: 80px; */
-  padding-bottom: 80px;
-
-  //centering
   display: flex;
-  /* justify-content: center; */
-
-  z-index: 2;
-
-  *,
-  & {
-    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
-  }
-
-  :hover {
-    z-index: 10;
-    box-shadow: rgba(23, 0, 102, 0.5);
-    transform: scale(1.1);
-  }
 `;
 
 const Card = styled.div`
   box-sizing: border-box;
-
   position: absolute;
-  width: 380px;
-  height: 280px;
+  width: 360px;
+  height: 265px;
 
-  background: rgba(181, 249, 253, 0.42);
+  background: rgba(151, 249, 255, 0.35);
   box-shadow: 0px 45.9087px 45.9087px rgba(69, 42, 124, 0.15);
   backdrop-filter: blur(20px);
   /* Note: backdrop-filter has minimal browser support */
 
   border-radius: 30px;
-
   align-items: center;
   align-content: center;
+
+  //above
+  z-index: 1;
+`;
+
+const Card2 = styled.div`
+  box-sizing: border-box;
+
+  position: absolute;
+  width: 300px;
+  height: 221px;
+
+  background: rgba(255, 255, 255, 0.4);
+  box-shadow: 0px 45.9087px 45.9087px rgba(69, 42, 124, 0.15);
+  backdrop-filter: blur(20px);
+  /* Note: backdrop-filter has minimal browser support */
+
+  border-radius: 30px;
+  align-items: center;
+  align-content: center;
+  z-index: 0;
 `;
 
 const CardBottom = styled.div`
   box-sizing: border-box;
 
   position: relative;
-  width: 380px;
-  height: 60px;
+  width: 360px;
+  height: 50px;
 
   background: rgba(0, 0, 0, 0.3);
   border-radius: 0px 0px 30px 30px;
 
-  top: 220px;
+  top: 215px;
 
   //centering
   align-items: center;
   display: grid;
+
+  z-index: 1;
+`;
+
+const CardBottom2 = styled.div`
+  box-sizing: border-box;
+
+  position: relative;
+  width: 300px;
+  height: 45px;
+
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 0px 0px 30px 30px;
+
+  top: 176px;
+
+  //centering
+  align-items: center;
+  display: grid;
+
+  @media (max-width: 900px) {
+    z-index: -2;
+  }
 `;
 
 const Subtitle = styled(BodyIntro)`
@@ -139,38 +177,4 @@ const Icon = styled.img`
   padding-top: 15px;
   margin-left: auto;
   margin-right: auto;
-`;
-
-const Card2 = styled.div`
-  box-sizing: border-box;
-
-  position: absolute;
-  width: 380px;
-  height: 280px;
-
-  background: rgba(255, 255, 255, 0.4);
-  box-shadow: 0px 45.9087px 45.9087px rgba(69, 42, 124, 0.15);
-  backdrop-filter: blur(20px);
-  /* Note: backdrop-filter has minimal browser support */
-
-  border-radius: 30px;
-  align-items: center;
-  align-content: center;
-`;
-
-const CardBottom2 = styled.div`
-  box-sizing: border-box;
-
-  position: relative;
-  width: 380px;
-  height: 60px;
-
-  background: rgba(0, 0, 0, 0.3);
-  border-radius: 0px 0px 30px 30px;
-
-  top: 220px;
-
-  //centering
-  align-items: center;
-  display: grid;
 `;
